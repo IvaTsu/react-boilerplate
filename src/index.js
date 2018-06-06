29821196da9bc92009aac8c31c2fd3c15a3ecb93
IvaTsu <ivan.tsud@gmail.com>
2018-06-06 09:54:22 +0300

    add preset options to the webpack config and init React component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-console.log('React Boilerplate');
-console.log('Another');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const DOCUMENT_ROOT = document.getElementById("root");
+
+ReactDom.render(
+    <div>Hello</div>,
+    DOCUMENT_ROOT,
+);
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,13 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ["babel-loader"]
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        'env',
+                        'react',
+                    ]
+                },
             }
         ]
     },
